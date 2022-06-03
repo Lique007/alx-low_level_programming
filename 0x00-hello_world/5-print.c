43a4d120665b6d7cb15f6efd02ab7f405fbeeaed
Angelique <angeliquehilario019@gmail.com>
2022-06-03 12:21:58 -0700

    6-size.c

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -2,10 +2,10 @@
 
 /**
  * main - program entry point
- * Return: 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}
+             printf("with proper grammar, but the outcome is a piece of art,\n");    
+                    return(0);
+} 
